7d34a3209d69307af6dbc8ce84fc2590841f6d52
Sabir777 <sabir.yanin2014@yandex.ru>
2024-04-06 22:23:08 +0300

    Создал папку Input - теперь все входные файлы сбрасываю туда, а выходные забираю из папки Output

--- 2-pdf-to-png-убрать-фон/убрать_фон.sh
+++ 2-pdf-to-png-убрать-фон/убрать_фон.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+# # Проверяем, существует ли каталог "Output"
+# if [ ! -d "Output" ]; then
+  # mkdir "Output"  # Создаем каталог, если его нет
+# fi
+
+# # Преобразую пдф в png с незначительным сжатием
+# # Удаляю фон
+# # Сохраняю файлы в парке Output
+# for file in $(ls Input/*.pdf); do
+  # file_output="${file%.pdf}.png"
+  # convert -density 100 "$file" -alpha off -fuzz 10% -transparent white -quality 90 "Output/$file_output"
+# done
+
+# # Перехожу в папку с созданными файлами и сжимаю их утилитой pngquant
+# cd Output
+# pngquant --quality=10-20 *.png --ext .png --force
+
+
 # Проверяем, существует ли каталог "Output"
 if [ ! -d "Output" ]; then
   mkdir "Output"  # Создаем каталог, если его нет
@@ -8,9 +26,10 @@ fi
 # Преобразую пдф в png с незначительным сжатием
 # Удаляю фон
 # Сохраняю файлы в парке Output
-for file in $(ls *.pdf); do
-  file_output="${file%.pdf}.png"
-  convert -density 100 "$file" -alpha off -fuzz 10% -transparent white -quality 90 "Output/$file_output"
+for file in Input/*.pdf; do
+  filename=$(basename "$file")
+  file_output="Output/${filename%.pdf}.png"
+  convert -density 100 "$file" -alpha off -fuzz 10% -transparent white -quality 90 "$file_output"
 done
 
 # Перехожу в папку с созданными файлами и сжимаю их утилитой pngquant
