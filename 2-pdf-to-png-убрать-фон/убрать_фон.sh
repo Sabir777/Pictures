ab3dd8910b88e46f7e737ab8f0373d3e8b77fbdc
Sabir777 <sabir.yanin2014@yandex.ru>
2024-04-06 21:25:01 +0300

    Задача выполнена: Преобразование из pdf в png с удалением фона и последующим сжатием

---
+++ 2-pdf-to-png-убрать-фон/убрать_фон.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Проверяем, существует ли каталог "Output"
+if [ ! -d "Output" ]; then
+  mkdir "Output"  # Создаем каталог, если его нет
+fi
+
+# Преобразую пдф в png с незначительным сжатием
+# Удаляю фон
+# Сохраняю файлы в парке Output
+for file in $(ls *.pdf); do
+  file_output="${file%.pdf}.png"
+  convert -density 100 "$file" -alpha off -fuzz 10% -transparent white -quality 90 "Output/$file_output"
+done
+
+# Перехожу в папку с созданными файлами и сжимаю их утилитой pngquant
+cd Output
+pngquant --quality=10-20 *.png --ext .png --force
+
