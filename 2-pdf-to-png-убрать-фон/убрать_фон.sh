28700929cf3683b7d2562d140dd5a2a3973b82ea
Sabir777 <sabir.yanin2014@yandex.ru>
2024-04-07 23:04:29 +0300

    Промежуточное сохранение. Пока ничего не понятно

--- 2-pdf-to-png-убрать-фон/убрать_фон.sh
+++ 2-pdf-to-png-убрать-фон/убрать_фон.sh
@@ -1,27 +1,7 @@
 #!/bin/bash
 
-# # Проверяем, существует ли каталог "Output"
-# if [ ! -d "Output" ]; then
-  # mkdir "Output"  # Создаем каталог, если его нет
-# fi
-
-# # Преобразую пдф в png с незначительным сжатием
-# # Удаляю фон
-# # Сохраняю файлы в парке Output
-# for file in $(ls Input/*.pdf); do
-  # file_output="${file%.pdf}.png"
-  # convert -density 100 "$file" -alpha off -fuzz 10% -transparent white -quality 90 "Output/$file_output"
-# done
-
-# # Перехожу в папку с созданными файлами и сжимаю их утилитой pngquant
-# cd Output
-# pngquant --quality=10-20 *.png --ext .png --force
-
-
-# Проверяем, существует ли каталог "Output"
-if [ ! -d "Output" ]; then
-  mkdir "Output"  # Создаем каталог, если его нет
-fi
+# Создаю папку "Output" если ее нет
+mkdir -p Output
 
 # Преобразую пдф в png с незначительным сжатием
 # Удаляю фон
