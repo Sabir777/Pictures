8f0e16d720d62e3f823b2fb898e0df4a3b964a17
Sabir777 <sabir.yanin2014@yandex.ru>
2024-04-08 22:07:49 +0300

    Рабочая версия: сжимает только файлы jpg, остальные файлы копируются. В папке Output полностью повторяется исходная структура из папки Input - куда помещается структура из папок с фотографиями

--- 3-уменьшить-фото-в-папках/сжать_фото.sh
+++ 3-уменьшить-фото-в-папках/сжать_фото.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# Создаю папку Output - если она не существует
-mkdir -p Output
+# Копирую структуру папок из папки Input в Output
+cd Input
+# Копирую только папки без файлов
+find . -type d -exec mkdir -p ../Output/{} \;
 
-# Копируем структуру папок из Input в Output
-cp -r Input/* Output/
 
-# Находим все файлы jpg в папке Output и её подпапках
-find Output -type f -name "*.jpg" | while read jpg_file; do
-    # Получаем имя папки, в которой находится файл
-    output_dir=$(dirname "$jpg_file")
-    # Сжимаем jpg файл программой ImageMagick
-    magick convert "$jpg_file" -resize 50% "$jpg_file"
+# Нахожу все файлы в папке Input рекурсивно
+# Если файл jpg - конвертирую его, если другого типа - копирую
+find . -type f | while read file; do
+  if [[ "$file" == *.jpg ]]; then
+    convert "$file" -resize 25% -quality 30 "../Output/$file"
+  else
+    cp "$file" "../Output/$file"
+  fi
 done
 
-# Копируем все файлы, не являющиеся jpg, из Input в Output
-find Output -type f ! -name "*.jpg" -exec cp --parents {} Output/ \;
