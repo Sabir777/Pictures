b95e422a29378079a0c5176d811fc7b702a6c184
Sabir777 <sabir.yanin2014@yandex.ru>
2025-03-17 20:22:27 +0300

    Исправил скрипт pictures.sh под новую структуру. Создана новая версия Pictures. Отмечаю тэгом

--- pictures.sh
+++ pictures.sh
@@ -18,44 +18,52 @@ tmux new-session -s Pictures -n pic-1 -d
 tmux send-keys -t Pictures 'cd 1-pdf-to-png-сжать ; vim pdf-to-png.sh' C-m
 
 
-# Запускаю 2-е окно: 2-pdf-to-png-убрать-фон
+# Запускаю 2-е окно: 2-pdf-to-png-белый-фон
 # создаю окно "pic-2" в сессии Pictures
 tmux new-window -n pic-2 -t Pictures
 
-# Открываю в vim "убрать_фон.sh"
-tmux send-keys -t Pictures:2.1 'cd 2-pdf-to-png-убрать-фон ; vim убрать_фон.sh' C-m
+# Открываю в vim "pdf-to-png-белый-фон.sh"
+tmux send-keys -t Pictures:2.1 'cd 2-pdf-to-png-белый-фон ; vim pdf-to-png-белый-фон.sh' C-m
 
 
-# Запускаю 3-е окно: 3-уменьшить-фото-в-папках
+# Запускаю 3-е окно: 3-pdf-to-png-убрать-фон
 # создаю окно "pic-3" в сессии Pictures
 tmux new-window -n pic-3 -t Pictures
 
-# Открываю в vim "сжать_фото.sh"
-tmux send-keys -t Pictures:3.1 'cd 3-уменьшить-фото-в-папках ; vim сжать_фото.sh' C-m
+# Открываю в vim "pdf-to-png-убрать-фон.sh"
+tmux send-keys -t Pictures:3.1 'cd 3-pdf-to-png-убрать-фон ; vim pdf-to-png-убрать-фон.sh' C-m
 
 
-# Запускаю 4-е окно: 4-добавить-пустое-место
+# Запускаю 4-е окно: 4-pdf-to-pdf-сжать
 # создаю окно "pic-4" в сессии Pictures
 tmux new-window -n pic-4 -t Pictures
 
-# Открываю в vim "добавить_место.sh"
-tmux send-keys -t Pictures:4.1 'cd 4-добавить-пустое-место ; vim добавить_место.sh' C-m
+# Открываю в vim "сжать_pdf.sh"
+tmux send-keys -t Pictures:4.1 'cd 4-pdf-to-pdf-сжать ; vim сжать_pdf.sh' C-m
 
 
-# Запускаю 5-е окно: 5-сжать-pdf
+# Запускаю 5-е окно: 5-pdf-to-pdf-белый-фон
 # создаю окно "pic-5" в сессии Pictures
 tmux new-window -n pic-5 -t Pictures
 
-# Открываю в vim "сжать_pdf.sh"
-tmux send-keys -t Pictures:5.1 'cd 5-сжать-pdf ; vim сжать_pdf.sh' C-m
+# Открываю в vim "улучшить_фон_сжать_pdf.sh"
+tmux send-keys -t Pictures:5.1 'cd 5-pdf-to-pdf-белый-фон ; vim улучшить_фон_сжать_pdf.sh' C-m
 
 
-# Запускаю 6-е окно: 6-преобразовать-pdf-в-png
+# Запускаю 6-е окно: 6-уменьшить-фото-в-папках
 # создаю окно "pic-6" в сессии Pictures
 tmux new-window -n pic-6 -t Pictures
 
-# Открываю в vim "сжать_pdf.sh"
-tmux send-keys -t Pictures:6.1 'cd 6-преобразовать-pdf-в-png ; vim конвертировать-pdf-в-png.sh' C-m
+# Открываю в vim "сжать_фото.sh"
+tmux send-keys -t Pictures:6.1 'cd 6-уменьшить-фото-в-папках ; vim сжать_фото.sh' C-m
+
+
+# Запускаю 7-е окно: 7-добавить-пустое-место
+# создаю окно "pic-7" в сессии Pictures
+tmux new-window -n pic-7 -t Pictures
+
+# Открываю в vim "добавить-пустое-место-картинки.sh"
+tmux send-keys -t Pictures:7.1 'cd 7-добавить-пустое-место ; vim добавить-пустое-место-картинки.sh' C-m
 
 
 # Подключаюсь к сессии "Pictures"      
