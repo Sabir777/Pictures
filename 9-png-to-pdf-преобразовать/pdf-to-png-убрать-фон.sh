a9c5331a88913cca3dc48a890d2802517d22d996
Sabir777 <sabir.yanin2014@yandex.ru>
2025-03-29 07:19:19 +0300

    Начал делать png-to-pdf

---
+++ 9-png-to-pdf-преобразовать/pdf-to-png-убрать-фон.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#-------------3.Удаление фона---------------#
+
+# Преобразовать pdf в папке Input в png с удалением фона
+# Результат в папке Output
+
+# Создаю папку "Output" если ее нет
+mkdir -p Output
+
+# Преобразую пдф в png с незначительным сжатием
+# Удаляю фон
+# Сохраняю файлы в парке Output
+for file in Input/*.pdf; do
+  filename=$(basename "$file")
+  file_output="Output/${filename%.pdf}.png"
+  magick -density 100 "$file" -alpha off -fuzz 10% -transparent white -quality 90 "$file_output"
+done
+
+# Перехожу в папку с созданными файлами и сжимаю их утилитой pngquant
+cd Output
+pngquant --quality=10-20 *.png --ext .png --force
+
