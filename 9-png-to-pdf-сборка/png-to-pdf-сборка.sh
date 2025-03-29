99a400a028019e84df4f0886ad87ee0b907da229
Sabir777 <sabir.yanin2014@yandex.ru>
2025-03-29 21:50:43 +0300

    Не работает скрипт: png-to-pdf-сборка. Первая версия - ищу ошибки

--- 9-png-to-pdf-сборка/png-to-pdf-сборка.sh
+++ 9-png-to-pdf-сборка/png-to-pdf-сборка.sh
@@ -11,9 +11,6 @@
 # Копирую структуру папок из папки Input в Output
 cd Input
 
-# Запоминаю полное имя базовой директории
-base_dir=$(pwd)
-
 # Копирую только папки без файлов
 find . -type d -exec mkdir -p ../Output/{} \;
 
@@ -23,25 +20,53 @@ find . -type f | while read file; do
 done
 
 
+# Базовая директория: папка Output
+cd ../Output
+
+# Запоминаю полное имя базовой директории
+base_dir=$(pwd)
+
 # Перебираю рекурсивно все папки: в каждой директории проверяю наличие png-файлов и собираю pdf-файл
 find . -type d | while read folder; do
 
     # Перехожу в каждую из папок
     cd "$folder"
 
-    # Собираю pdf-файл
-    # ???
+    # Получаю имя папки
+    name_dir=$(basename "$folder")
 
-    # Если я нахожусь в папке типа *.pdf или *.PDF то перемещаю файл на уровень вверх
-    # ???
+    # Создаю массив из имен png-файлов
+    png_arr=($(ls *.png *.PNG | sort -V))
+
+    # Если в папке нет png-файлов: прокрутка цикла
+    if [[ ${#png_arr[@]} -eq 0 ]]; then
+        cd "$base_dir"
+        continue
+    fi
+
+    # Имя выходного файла
+    output_file="output.pdf"
+
+    # Проверяю соответствие текущей папки шаблону
+    if [[ "$name_dir" == *.pdf || "$name_dir" == *.PDF ]]; then
+        output_file="$name_dir"
+
+        # Собираю pdf-файл
+        magick "${png_arr[@]}" "$output_file"
+        # Перемещаю pdf-файл на уровень вверх
+        mv "$output_file" "../$output_file"
+    else
+        # Собираю pdf-файл
+        magick "${png_arr[@]}" "$output_file"
+    fi
 
     # Удаляю png-файлы
     rm *.png *.PNG
 
-    # Если папка пуста удаляю данную папку
-    # ???
-
     # возвращаюсь в базовую директорию
     cd "$base_dir"
+
+    # Если папка пуста удаляю данную папку
+    rmdir "$folder" 2>/dev/null
 done
 
