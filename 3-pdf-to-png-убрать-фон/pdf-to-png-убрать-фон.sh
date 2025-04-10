7b7d463b5d255d5c2669386c29bdc9d900393fb4
Sabir777 <sabir.yanin2014@yandex.ru>
2025-04-10 21:27:42 +0300

    3-pdf-to-png-убрать-фон: сделал фиксированный выходной размер подписи

--- 3-pdf-to-png-убрать-фон/pdf-to-png-убрать-фон.sh
+++ 3-pdf-to-png-убрать-фон/pdf-to-png-убрать-фон.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
 
-#-------------3.Удаление фона---------------#
+#-------------3.Удаление фона и масштабирование PNG---------------#
 
 # Преобразовать pdf в папке Input в png с удалением фона
+# и масштабированием по ширине с сохранением пропорций
 # Результат в папке Output
 
 # Создаю папку "Output" если ее нет
 mkdir -p Output
 
-# Преобразую пдф в png с незначительным сжатием
-# Удаляю фон
-# Сохраняю файлы в парке Output
+# Ширина изображения (в пикселях) - для большей четкости лучше 1024, с последующим уменьшением до 150
+WIDTH=150
+
+# Ширина изображения (в пикселях) - параметр задается так: -resize x${HEIGHT} \
+# HEIGHT=768
+
+# Преобразую pdf в png, удаляю фон, масштабирую по ширине
 for file in Input/*.pdf; do
   filename=$(basename "$file")
   file_output="Output/${filename%.pdf}.png"
-  magick -density 100 "$file" -alpha off -fuzz 10% -transparent white -quality 90 "$file_output"
+  magick -density 100 "$file" \
+    -alpha off \
+    -fuzz 10% -transparent white \
+    -resize ${WIDTH}x \
+    -quality 90 "$file_output"
 done
 
-# Перехожу в папку с созданными файлами и сжимаю их утилитой pngquant
+# Перехожу в Output и сжимаю PNG
 cd Output
 pngquant --quality=10-20 *.png --ext .png --force
-
